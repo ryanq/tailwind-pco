dd7deb2bfbe937e3fc04f778670efc273602e145
Ryan Quattlebaum <ryan.quattlebaum@icloud.com>
2021-02-28 16:16:50 -0500

    Import TailwindCSS

---
+++ pages/dashboard.jsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Planning Center Services</title>
+      </Head>
+
+      <h1 className="text-6xl font-bold text-gray-900">Tailwind says "Hello!"</h1>
+    </div>
+  );
+}
