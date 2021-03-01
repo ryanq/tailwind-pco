5744ffdce9161fa3c71d79160b8724cf0cfcb865
Ryan Quattlebaum <ryan.quattlebaum@icloud.com>
2021-03-01 09:32:11 -0500

    Dashboard page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,29 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          lightest: "hsl(88, 24%, 92%)",
+          lighter: "hsl(88, 32%, 86%)",
+          light: "hsl(88, 40%, 54%)",
+          DEFAULT: "hsl(88, 44%, 40%)",
+          dark: "hsl(88, 46%, 36%)",
+          darker: "hsl(88, 52%, 32%)",
+          darkest: "hsl(88, 72%, 20%)",
+        },
+        green: colors.lime,
+      },
+      fontSize: {
+        "2xs": ".625rem",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
